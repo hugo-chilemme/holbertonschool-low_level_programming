399ebd0bd9becc208d7b1790b80ff03480e2bfcb
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-09-30 01:46:35 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - description pour main
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
