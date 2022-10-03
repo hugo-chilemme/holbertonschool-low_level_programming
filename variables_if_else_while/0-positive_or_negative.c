be21d61331a534f2aa3f33a708061a6458070d9c
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-03 01:43:47 -0700

    Folder

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n > 0) {
-		printf("is positive");
+		printf("%d is positive", n);
 	} else if (n < 0) {
-		printf("is negative");
+		printf("%d is negative", n);
 	} else {
-		printf("is zero");
+		printf("%d is zero", n);
 	}
 	return (0);
 }
