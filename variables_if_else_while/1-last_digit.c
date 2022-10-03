5faec3d67a5d6ddb5b6fe8593677c226b632cf2a
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-03 03:12:40 -0700

    Calcul digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int lg;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lg = n % 10;
+	printf("Last digit of %d is %d ", n, lg);
+	if (lg > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (lg == 0) 
+	{
+		printf("and is 0\n");
+	}
+	else if (lg < 6 && lg != 0) 
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
