0674452dd08607e1c3b18e88f50a4bb10557207a
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-04 04:43:00 -0700

    Print base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	for (; num <= 102; num+=1)
+	{
+		putchar(num);
+		if (num == 57)
+			num = 97; 
+	}
+	putchar('\n');
+	return (0);
+}
