454f96d0824df3a07e0b1a564ab93ddb1965b6d4
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-04 04:51:16 -0700

    Print comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	for (; num < 57; num++)
+	{
+		putchar(num);
+		putchar(44);
+		putchar(160);
+	}
+	putchar('\n');
+	return (0);
+}
