a55559952d076ec118491273f1b6a45748c98c34
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-04 04:31:31 -0700

    Revert alphabetics

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 122;
+
+	for (; num >= 97; num-=1)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
