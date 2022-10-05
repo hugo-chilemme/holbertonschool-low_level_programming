3c30c330100f057b2d913661cdf9ced9b8e4d414
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-05 09:25:48 +0200

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int asciiCode[] = {95, 112, 117, 116, 99, 104, 97, 114}; // _putchar
+	int asciiCode[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i = 0;
 	
 	for(; i < strlen(asciiCode); ++i) {
