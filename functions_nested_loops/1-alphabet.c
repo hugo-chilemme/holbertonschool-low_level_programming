af1d42d7be5cc2d8f01306a249c76f8a0166c81e
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-05 01:01:10 -0700

    Adding break line

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -9,11 +9,11 @@
  */
 void print_alphabet(void)
 {
-        int i = 97;
+	int i = 97;
 
-        for (; i <= 122; i++)
-        {
-                _putchar(i);
-        }
+	for (; i <= 122; i++)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
