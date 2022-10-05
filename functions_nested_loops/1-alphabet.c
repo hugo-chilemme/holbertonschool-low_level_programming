09791ddd0f85e160d3f983ba7d0220f4fd730bb7
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-05 09:37:39 +0200

    Print alphabet with c function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
