01c16cf7d9bbc19a50399fa8facde8f916a08bb6
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-06 06:34:38 -0700

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * @a : First Input int
+ * @b : Second input int
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c = 48;
+
+	for (; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
