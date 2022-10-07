5cd1df9df6948c1ae01d502e5a305b110c0ef763
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-07 00:38:17 -0700

    Echelle

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n : number
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+
+	int i = 0;
+	int p = 0;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (; i < n; i++)
+	{
+		int c = 0;
+
+		for (; c < p; c++)
+		{
+			_putchar(' ');
+		}
+		p++;
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
