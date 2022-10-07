bb454aea1de4be0a2008eaac8cd7acc3e20e5aeb
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-07 00:43:25 -0700

    Print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * @size : size of square
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+
+	int i = 0;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+
+	for (; i < size; i++)
+	{
+		int s = 0;
+
+		for (; s < size; s++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
