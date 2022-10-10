f4536264192fea9f33f51696ce1f4f8c819ba7fa
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-10 12:02:36 -0700

    THE POINTERS

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: input char
+ *
+ * Return: Always 1 (True) or 0 (False)
+ */
+void reset_to_98(int *p)
+{
+	*p = 98;
+}
