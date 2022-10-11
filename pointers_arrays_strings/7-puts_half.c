e9cdeaa41bd63224566e91a49802014c0c91579d
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-11 05:57:24 -0700

    Puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - point
+ * @str : char of string
+ * Return: Always 1 (True) or 0 (False)
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int s = 0;
+
+	for (; str[len] != 0; len++);
+
+	s = len / 2;
+
+	for (; s < len; s++)
+		_putchar(str[s]);
+	_putchar('\n');
+}
