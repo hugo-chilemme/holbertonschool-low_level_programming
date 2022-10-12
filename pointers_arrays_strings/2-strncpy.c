eb3d38353244943d469eff01ce988f568c41b792
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-12 02:10:50 -0700

    Path try error

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -25,7 +25,7 @@ char *_strncpy(char *dest, char *src, int n)
 		calc_dest++;
 
 
-	if (stop_at <= calc_dest)
+	if (stop_at <= calc_dest && calc_dest >= stop_at)
 	{
 		for (; index <= calc_src; index++)
 		{
