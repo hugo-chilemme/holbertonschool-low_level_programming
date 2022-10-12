f97eab18829aa0a9bba856bf9556449b59f5cd7f
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-12 02:05:01 -0700

    Strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,28 +11,21 @@
  *
  * Return: Always 1 (True) or 0 (False)
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int calc_dest = 0;
 	int calc_src = 0;
-	int output_size = 0;
 	int index = 0;
 	int stop_at = n;
-
-	while (dest[calc_dest] != '\0')
-		calc_dest++;
 
 	while (src[calc_src] != '\0')
 		calc_src++;
 
-	output_size = calc_dest + calc_src;
 
-
-	for (; index < output_size; index++)
+	for (; index <= calc_src; index++)
 	{
-		if (index >= calc_dest && stop_at > 0)
+		if (stop_at > 0)
 		{
-			dest[index] = src[index - calc_dest];
+			dest[index] = src[index];
 			stop_at--;
 		}
 	}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
