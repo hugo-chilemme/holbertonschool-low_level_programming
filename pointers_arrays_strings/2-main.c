09d3e4265156c8092302ac3ba0a8c0661ac65f89
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-12 11:25:43 -0700

    Reverse array

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -1,41 +1,33 @@
-#include "main.h"
 #include <stdio.h>
 
+char *_strncpy(char *, char *, int);
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char s1[98];
-    char *ptr;
-    int i;
+	char s1[98];
+	char *p;
+	int i;
 
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
-    return (0);
+	for (i = 0; i < 98 - 1; i++)
+		s1[i] = '-';
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	p = _strncpy(s1, "", 4);
+	printf("%s\n", s1);
+	printf("%s\n", p);
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
+			printf(" ");
+		if (!(i % 10) && i)
+			printf("\n");
+		printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+	return (0);
 }
---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: dest input
+ * @b: lenght
+ *
+ * Return: Always 1 (True) or 0 (False)
+ */
+void reverse_array(int *a, int n)
+{
+	int index = 0;
+	int e = n - 1;
+
+	for (; index < n / 2; index++)
+	{
+		char cache = a[index];
+		a[index] = a[e - index];
+		a[e - index] = cache;
+	}
+}
