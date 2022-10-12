bcbd3c5da8fbfe4f46c8ad6f9362a11713326360
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-12 00:51:29 -0700

    Betty correction

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -14,12 +14,12 @@ char *_strcpy(char *dest, char *src)
 	int len = 0;
 	int i = 0;
 
-	while (src[len] != 0) 
+	while (src[len] != 0)
 		len++;
 
 
 	for (; i < len; i++)
 		dest[i] = src[i];
 
-	return (dest);	
+	return (dest);
 }
