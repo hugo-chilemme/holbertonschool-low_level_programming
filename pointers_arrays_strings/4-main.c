5ba4a905da094e713f7e51a935098571afc976fa
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-12 01:05:06 -0700

    I search the error :thinking:

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -9,7 +9,9 @@ int main(void)
 {
     char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    str = "Holberton!";
+
+    printf("%s\n", str);
     print_rev(str);
     return (0);
 }
