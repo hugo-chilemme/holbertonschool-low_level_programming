e454c056f3532e99d099369fd81f099cbccb04fa
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-13 00:33:50 -0700

    Patch merge

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,9 +8,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-<<<<<<< HEAD
 int _strcmp(char *s1, char *s2);
-=======
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
@@ -19,4 +17,4 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *n);
 char *cap_string(char *n);
->>>>>>> 0e2700b2cec928f43faf112f069a83dc1cc0415c
+
