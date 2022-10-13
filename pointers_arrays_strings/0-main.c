c40acd4fb6fac6845617f295e76ea4a50e3f82f5
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-13 00:36:14 -0700

    98

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -2,21 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code 
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+    int n;
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
     return (0);
 }
--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -11,5 +11,5 @@
  */
 void reset_to_98(int *p)
 {
-	p = 98;
+	*p = 98;
 }
