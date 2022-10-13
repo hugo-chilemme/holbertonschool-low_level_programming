3145a1d7a6f39e333a82787b5f1629ce06edce98
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-13 07:32:41 -0700

    print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,12 @@
 void print_rev(char *s)
 {
 	int i;
-
-	while (s[i] != '\0')
-		i++;
-
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
 	for (i = i - 1; s[i] != '\0'; i--)
+	{
 		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
