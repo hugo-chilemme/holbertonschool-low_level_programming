9ec34321ea28576d4e9f1362a9ba1bbd4ec2711a
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-14 05:10:36 -0700

    Path betty

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - compare number of character
+ * @s1: dest
+ * @s2: src
+ *
+ * Return: s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
 
+	while (s2[i] != '\0' && s1[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
