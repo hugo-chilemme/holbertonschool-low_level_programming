ca3b1fa22c656e5ad3155b9a60c42ad759770171
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-14 05:56:34 -0700

    strspn

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _strpbrk - Function
- * @s: dest
- * @accept: accept char
+ * _strpbrk - scan the 1 first occurence of any character who is part of 2 and
+ * return the character from 1.
+ * @s: is the one who will be scanned
+ * @accept: search between this list of character
  *
- * Return: Always(1)
+ * Return: s or null
  */
 char *_strpbrk(char *s, char *accept)
 {
@@ -15,10 +16,14 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				break;
+			}
 		}
 		if (s[i] == accept[j])
+		{
 			return (s + i);
+		}
 	}
-	return (NULL);
+	return ('\0');
 }
