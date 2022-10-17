4e95889d27f77368e14460d26e152c473a071648
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-17 02:01:32 -0700

    Task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: input
+ *
+ * Return: Always 1 (True) or 0 (False)
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
