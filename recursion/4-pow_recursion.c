56be512eb5d84d77149e5ad3961180c518506c11
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-17 06:29:21 -0700

    pow

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -9,9 +9,9 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y <= 0)
-		return (-1);
-
-	y--;
-	return (x * _pow_recursion(x, y));
+	if (y == 0)
+		return (1);
+	if (y < 0) 
+		return (-1);	
+	return (x * _pow_recursion(x, y - 1));
 }
