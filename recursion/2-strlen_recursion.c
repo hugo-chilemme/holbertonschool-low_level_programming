1eacf229ef843eea1f90595ecfaffbd646f287bd
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-17 05:17:00 -0700

    strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * strlen_recursion - Entry point
+ * @s: input
+ *
+ * Return: Always 1 (True) or 0 (False)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return 0;
+
+	return (1 + _strlen_recursion(s + 1));
+}
