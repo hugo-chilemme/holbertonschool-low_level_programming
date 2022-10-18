dde04ea1d155d0a851a47bcf745926fcff1baa18
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-18 11:40:58 -0700

    Patch Betty requirement

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,12 +2,14 @@
 
 /**
  * main - check the code
+ * @argc: number of argv
+ * @argv: array of argument
  *
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-    if(argc)
-    	printf("%s\n", argv[0]);   
-    return (0);
+	if (argc)
+		printf("%s\n", argv[0]);
+	return (0);
 }
