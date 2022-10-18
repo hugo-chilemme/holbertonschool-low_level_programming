8f726954813ff6321dc0a682107ef171ab9e291a
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-18 11:39:28 -0700

    Display program name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+    if(argc)
+    	printf("%s\n", argv[0]);   
+    return (0);
+}
