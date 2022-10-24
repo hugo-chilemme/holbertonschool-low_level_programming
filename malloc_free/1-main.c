22db3bcf32f7e86ff0f38656ceab7d16e7b335dd
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-24 12:48:38 -0700

    concat

--- malloc_free/1-main.c
+++ malloc_free/1-main.c
@@ -1,23 +1,23 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+char *_strdup(char *str);
+
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = _strdup("Duplicated");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	s = _strdup(NULL);
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	free(s);
+	return (0);
 }
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat - point
+ * @s1 :copy this string
+ * @s2: with this
+ *
+ * Return: Always 1 (True) or 0 (False)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ret;
+	int len_1 = 0;
+	int len_2 = 0;
+	int index = 0;
+
+	while (s1[len_1])
+		len_1++;
+
+	while (s2[len_2])
+		len_2++;
+
+	ret = malloc(sizeof(char) * (len_1 + len_2));
+
+	for (; index <= (len_1 + len_2); index++)
+	{
+		if (index <= len_1)
+			ret[index] = s1[index];
+		else
+			ret[index] = s2[index - len_1 - 1];
+	}
+
+
+
+	return (ret);
+}
