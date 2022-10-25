320d856f205cc62316d897a70c075a449380daca
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-25 01:06:29 -0700

    calcul problem

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int len_2 = 0;
 	int index = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+	       	s1 = "";
+       	if (s2 == NULL)
+	       	s2 = "";
 
 	while (s1[len_1])
 		len_1++;
