0e11a3cf4801cc86cd7d8e0c3e5b6e6566005aec
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-25 11:42:11 -0700

    address

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - point
+ * @b : size of malloc
+ * Return: Always 1 (True) or 0 (False)
+ */
+void *malloc_checked(unsigned int b)
+{
+	if (b > 0)
+	{		
+		void *loc = malloc(b);
+		if (loc == 0)
+			exit(98);
+		return (loc);
+	}
+	exit(98);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+# Hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo w____ ? 
