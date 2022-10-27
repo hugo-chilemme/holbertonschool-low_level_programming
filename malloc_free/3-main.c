cee6f860ef5a6f9710dc8621abdab3aa9bfea1f6
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-27 01:55:22 -0700

    test

--- malloc_free/3-main.c
+++ malloc_free/3-main.c
@@ -1,7 +1,6 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-int **alloc_grid(int, int);
 
 /**
  * print_grid - prints a grid of integers
@@ -13,43 +12,41 @@ int **alloc_grid(int, int);
  */
 void print_grid(int **grid, int width, int height)
 {
-	int w;
-	int h;
+    int w;
+    int h;
 
-	h = 0;
-	while (h < height)
-	{
-		w = 0;
-		while (w < width)
-		{
-			printf("%d ", grid[h][w]);
-			w++;
-		}
-		printf("\n");
-		h++;
-	}
+    h = 0;
+    while (h < height)
+    {
+        w = 0;
+        while (w < width)
+        {
+            printf("%d ", grid[h][w]);
+            w++;
+        }
+        printf("\n");
+        h++;
+    }   
 }
 
 /**
- * main - check the code .
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int **grid;
-	int h;
-	int w;
+    int **grid;
 
-	h = 20;
-	w = 20;
-	grid = alloc_grid(h, w);
-	if (grid == NULL)
-	{
-		return (1);
-	}
-	print_grid(grid, h, w);
-	printf("\n");
-	free(grid);
-	return (0);
+    grid = alloc_grid(48, 32);
+    if (grid == NULL)
+    {
+        return (1);
+    }
+    print_grid(grid, 6, 4);
+    printf("\n");
+    grid[0][3] = 98;
+    grid[3][4] = 402;
+    print_grid(grid, 6, 4);
+    return (0);
 }
