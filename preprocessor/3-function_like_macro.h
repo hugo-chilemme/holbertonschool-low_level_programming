fd2f5e3aa0a22b665d432436bfd0b8cf67f05eec
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-27 00:56:16 -0700

    Function ABS in header

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,1 +1,6 @@
-
+int ABS(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
--- preprocessor/3-main.c
+++ preprocessor/3-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
 
 /**
  * main - check the code
