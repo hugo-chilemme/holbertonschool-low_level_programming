d85b37ff3b8ed0a4f85dba0b365d958d8cecce3a
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-27 02:01:42 -0700

    free gridd

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - point
+ * @grid: width
+ * @height: height
+ *
+ * Return: Always 1 (True) or 0 (False)
+ */
+void free_grid(int **grid, int height)
+{
+	int fr = 0;
+	for (; fr <= height; fr++)
+		free(grid[fr]);
+	free(grid);
+}
