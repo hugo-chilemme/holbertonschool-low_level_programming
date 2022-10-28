576282fd6fb09ed1f25c2dda7304c2f24d2cb211
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-28 18:58:20 +0200

    Create README.m

---
+++ malloc_free/README.m
@@ -0,0 +1,6 @@
+[Back to home](https://github.com/HugoCLI/holbertonschool-low_level_programming/blob/main/README.md)
+
+# C - malloc, free
+###### Training by Holberton School ([www.holbertonschool.com](www.holbertonschool.com))
+&nbsp;
+- [0. Create your first Makefile.](0-Makefile)
