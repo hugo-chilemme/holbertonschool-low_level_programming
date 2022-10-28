7dd372541d07c2fa76ce9dc7b257595ff7e745c4
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-10-28 06:32:28 -0700

    nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,8 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
         unsigned int totalSize = 0;
         unsigned int index = 0;
 	
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+			
+	if (s2 == NULL)
+		s2 = "";
 
         while (s1[len_s1] != '\0')
                 len_s1++;
