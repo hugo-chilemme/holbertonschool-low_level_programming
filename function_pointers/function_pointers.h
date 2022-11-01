85e0336944a6ae7f0943345f6306eaf606283b51
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-01 09:16:31 -0700

    Task 3

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -4,24 +4,4 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
-
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 #endif
