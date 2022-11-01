53a18d3a89bed2a47e7551be855646f5654c5668
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-01 06:10:15 -0700

    0 and 1

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+struct dog
+{
+	/**
+	 * @name: name of the dog
+	 */
+	char *name;
+	/** 
+	 * @age: the dog age
+	 */
+	float age;
+	/**
+	 * @owner: name of the dog owner
+	 */
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
