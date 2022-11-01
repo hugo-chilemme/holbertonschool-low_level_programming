d4fea4f42c43d6aa566e5a2a9b0d1f194e13da5e
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-01 08:42:35 -0700

    betty correction

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,16 @@
 #ifndef DOG_FILE
 #define DOG_FILE
+/**
+ * struct dog - Struct dog
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
 typedef struct dog
 {
-	/**
-	 * @name: name of the dog
-	 */
 	char *name;
-	/** 
-	 * @age: the dog age
-	 */
 	float age;
-	/**
-	 * @owner: name of the dog owner
-	 */
 	char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
