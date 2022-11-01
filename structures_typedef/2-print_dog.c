d434d389ba7286e8eb9e9b94139ed2e55ec55dfe
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-01 06:21:43 -0700

    table

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,10 +11,7 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
-		printf("(nil)");
 		return;
-	}
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
