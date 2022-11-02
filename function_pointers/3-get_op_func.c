0c74343377ef129bfadeea822ea4b593c96e3012
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-02 00:54:54 -0700

    Update

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,17 +19,14 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	i = ops[0].op == s ? 0 : i;
-	i = ops[1].op == s && i == 0 ? 1 : i;
-	i = ops[2].op == s && i == 0 ? 2 : i;
-	i = ops[3].op == s && i == 0 ? 3 : i;
-	i = ops[4].op == s && i == 0 ? 4 : i;
-	i = ops[5].op == s && i == 0 ? 5 : i;
-
-	if (i == 5)
+	while (i < 5)
 	{
-		printf("Error\n");
-		exit(98);
+		if (*ops[i].op == *s)
+			return (ops[i].f);
+		i++;
 	}
+
+	printf("Error\n");
+	exit(98);
 	return (ops[i].f);
 }
