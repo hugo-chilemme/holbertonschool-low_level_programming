94a0b5cae7636eccef0609ea80f280ae5da68436
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-05 14:07:26 -0700

    ooof

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,5 +5,16 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
+void print_all(const char * const format, ...);
+/**
+ * struct formats - Struct dog
+ *
+ * @t: first letter of the type
+ * @f: function execute per type
+ */
+typedef struct formats
+{
+	char *t;
+	void (*f)(va_list p);
+} form;
 #endif
