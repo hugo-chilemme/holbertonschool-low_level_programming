f9404a60f1bbce4b6f05b8692265c9523c35d320
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-05 06:51:40 -0700

    task 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,15 +3,16 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- * new_dog - Entry point
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
+ * free_dog - Entry point
+ * @d: dog structure
  * Return: Always 0.
  */
 void free_dog(dog_t *d)
 {
+	if (!d)
+		return;
 	free(d->name);
 	free(d->owner);
+	free(d);
 }
 
