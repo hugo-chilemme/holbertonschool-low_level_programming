07cfded69aa72f931370ad0a2862874531c4db46
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-05 02:59:12 -0700

    Task 2

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,6 +4,6 @@
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
