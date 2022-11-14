9d01ba88e052cbd90d7bb0bc368612e2c346047b
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-14 00:46:52 -0800

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,15 +8,13 @@
  */
 size_t print_list(const list_t *h)
 {
-	int len = 0;
 	if(!h)
 		return (0);
-	
-	len = h->len;
-	if (!h->str)
-		len = 0;
 
-	printf("[%i] %s\n", len, h->str);
+	if (!h->str)
+		printf("[0] (nil)\n");
+	else 
+		printf("[%i] %s\n", h->len, h->str);
 
 	if (h->next)
 		return (1 + print_list(h->next));
