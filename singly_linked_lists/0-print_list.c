0828b73634a51c07f591ad3215f1c64130de187b
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-14 00:42:22 -0800

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - Function
+ * @h: linked list
+ * Description: print a linked list
+ * Return: length of linked list
+ */
+size_t print_list(const list_t *h)
+{
+	int len = h->len;
+
+	if (!h->str)
+		len = 0;
+
+	printf("[%i] %s\n", len, h->str);
+
+	if (h->next)
+		return (1 + print_list(h->next));
+	return (1);
+}
