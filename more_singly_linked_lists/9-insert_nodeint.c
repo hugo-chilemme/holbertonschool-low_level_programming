5a358fd48c4391713fa40ca8c6777c18e2bfbd65
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-16 02:39:19 -0800

    9-insert_nodeint.c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -43,7 +43,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		checker = checker->next;
 		seek++;
 	}
-	checker->next = newNode;
-
-	return (newNode);
+	return (NULL);
 }
