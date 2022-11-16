16ea88560be4b013164e202c47df1edb35855071
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-16 01:38:32 -0800

    6-pop_listint.c

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Function
+ * @head: nodelist
+ * Return: 0
+ * Description: Delete first element and return node list remaining
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *target = *head;
+	listint_t *suite;
+	int cache_num;
+
+	if (!head)
+		return (0);
+
+	if (target->next)
+	{
+		suite = target->next;
+		cache_num = target->n;
+
+		free(target);
+		*head = suite;
+		return (cache_num);
+	}
+	return (0);
+
+}
