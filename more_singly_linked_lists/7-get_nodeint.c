b114d2cad423d6d299027f3750b64bb56f2d99fa
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-16 02:10:22 -0800

    betty correction: arguments missing

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -3,6 +3,7 @@
 /**
  * get_nodeint_at_index - Function
  * @head: nodelist
+ * @index: index of node target
  * Return: index
  * Description: Return index of element (if not found return null);
  */
@@ -18,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (checker);
 
 	while (checker->next)
-	{		
+	{
 		checker = checker->next;
 		count++;
 		if (count == index)
