1bd0e551a692c99613c2dcfc9ed3758967cf22ef
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-16 01:00:37 -0800

    Create 2-add_nodeint.c

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,4 +19,5 @@ typedef struct listint_s
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
