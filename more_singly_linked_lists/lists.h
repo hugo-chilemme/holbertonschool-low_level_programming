0252411d7cede63493eb49ff71bfd2582576ea17
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-16 00:47:42 -0800

    betty space correction

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -9,12 +9,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 #endif
