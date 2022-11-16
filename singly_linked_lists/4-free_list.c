926c2e6b3364a644d3df99c65a816bd709240b2b
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-16 00:36:04 -0800

    free_list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,22 +1,16 @@
 #include "lists.h"
 
 /**
- * add_node_end - Function
- * @head: linked list
- * @str: add this new element
+ * free_list - Function
+ * @head: node list
  * Description: add element in a  linked list
- * Return: the new element
+ * Return: void
  */
 void free_list(list_t *head)
 {
-	list_t *cache = head;
-	
-	while (cache->next)
-	{
-		if (!cache->next)
-			free(cache);
+	if (head->next)
+		free_list(head->next); 
 
-		cache = cache->next;
-	}	
+	free(head->str);
 	free(head);
 }
