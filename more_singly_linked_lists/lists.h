cdeb4a2cc93622abdc7868126bb4a034399a9cf3
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-16 01:12:08 -0800

    3-add_nodeint_end.c

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -17,7 +17,10 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif
