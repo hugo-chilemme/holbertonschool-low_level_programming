bcf7f5ed552e928a50e3c302131d50f8a17237e8
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-18 07:04:52 -0800

    Adding _putchar inmain.h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -5,5 +5,9 @@
 #include <string.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+void getBin(int num, char *str);
+int _pow_recursion(int x, int y);
 
 #endif
