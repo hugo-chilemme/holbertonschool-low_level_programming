e08fa8ff669c94bf61b1f5fd8d78f96f34b70500
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-21 02:39:37 -0800

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function
+ * @filename: Name of the target file
+ * @letters: number letters to print
+ * Description: Return number of letters
+ * Return: Result
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file = open(filename, O_RDONLY);
+	char *str = malloc(sizeof(char) * letters);
+
+	if (file == -1)
+		return (0);
+
+	read(file, str, letters);
+	str[letters] = '\0';
+
+	close(file);
+	printf("%s", str);
+
+	return (letters);
+}
