b32c6dd97a39d1fbcb54320c2094a267f9100355
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-21 02:40:41 -0800

    0-read_textfile.c

--- file_io/main.h
+++ file_io/main.h
@@ -1,8 +1,16 @@
 #ifndef MAIN
 #define MAIN
 
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#ifndef
+#endif
