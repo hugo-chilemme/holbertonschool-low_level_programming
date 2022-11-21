8e74b3e2609ef18a35b14121f7d840316031d0c9
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-21 03:00:25 -0800

    0-read_textfile.c

--- file_io/0-main.c
+++ file_io/0-main.c
@@ -16,9 +16,8 @@ int main(int ac, char **av)
         dprintf(2, "Usage: %s filename\n", av[0]);
         exit(1);
     }
-    n = read_textfile(av[1], 114);
+    n = read_textfile(av[1], 2000);
     printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
+    
     return (0);
 }
