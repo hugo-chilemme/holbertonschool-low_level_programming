dd281c6d69f89a970e6ba9fabf906e92676e5b84
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-22 13:18:36 -0800

    Fix files issues & Adding dblist_int function

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Function
+ * @h: node element
+ * Return: Number of element in @h.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int Index = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		Index += 1;
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+
+	return (Index);
+}
