bccc0d6d8e2efda1e29cca286ea2afb9e2dd407d
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-22 13:49:41 -0800

    Fix: betty correction

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,11 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 
-	if (*head) 
+	if (*head)
 	{
 		new->next = *head;
 		(*head)->prev = new;
 	}
 	*head = new;
-	return (new);	
+	return (new);
 }
