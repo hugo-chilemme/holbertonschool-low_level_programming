5539cbc9b66880f984276f1b139c220e6472bcad
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-22 00:35:34 -0800

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - Function
+ * @filename: Name of the target file
+ * @text_content: write this content in a file
+ * Return: 1: Success -1: Error
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_WRONLY, 0300);
+
+	if (file == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	write(file, text_content, strlen(text_content));
+	close(file);
+	return (1);
+}
