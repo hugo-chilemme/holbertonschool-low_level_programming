20b42912f3ca25f616508f3ab321d86a3aac1513
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-22 00:39:10 -0800

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
