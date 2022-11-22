c0f8b41dcb43ba3cf7477a461ea273c0495e0deb
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-22 01:19:30 -0800

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file;
 	char *copy;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 
 	file = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0664);
