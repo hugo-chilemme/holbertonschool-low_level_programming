305edcb4846ad001e52c7368302165ffd35fcaf9
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-27 04:07:49 -0800

    Betty correction

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,6 +1,10 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_create - Function
+ * @size: size of array
+ * Return: hash tables
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *array = malloc(sizeof(hash_table_t));
