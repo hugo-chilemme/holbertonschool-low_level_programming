d05a715cf0592628e968888006a5cb5f5118be1b
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-27 04:06:40 -0800

    0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *array = malloc(sizeof(hash_table_t));
+
+	if (!array || size == 0)
+	{
+		free(array);
+		return (NULL);
+	}
+
+	array->size = size;
+	array->array = malloc(sizeof(hash_table_t *) * size);
+	if (!array->array)
+	{
+		free(array->array);
+		free(array);
+		return (NULL);
+	}
+	return (array);
+}
