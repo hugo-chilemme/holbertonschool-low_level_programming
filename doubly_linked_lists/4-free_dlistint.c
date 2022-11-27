d7f63d54a0b30a51faa91e2b946834f27b1bac82
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-27 01:34:28 -0800

    Fix issue: betty correction

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,14 @@
 #include "lists.h"
 
 
-
+/**
+ * free_dlistint - Function
+ * @head: node element
+ * Return: Nothing.
+ */
 void free_dlistint(dlistint_t *head)
 {
-	
+
 	while (head)
 	{
 		if (head->prev)
