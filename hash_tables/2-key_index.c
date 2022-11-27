7b21457f5b5b3077af647439bed24f54f91bd58b
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-27 04:14:47 -0800

    betty correction

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - Function
+ * @key: value of element
+ * @size: size
+ * Return: hash_djb2 value
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
