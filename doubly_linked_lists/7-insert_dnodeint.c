288c1f9f8eba3fa0c38cf3dc50c5f17f333ed2eb
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-11-27 02:29:54 -0800

    Fix all cases

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		checker = checker->next;
 		seek++;
 
-		if (!checker && seek == idx)
+		if (!checker && seek <= idx)
 		{
 			newNode->prev = lastNode;
 			lastNode->next = newNode;
