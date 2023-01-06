77a13cd7fec4223183ab4b44b173464f4fcb871e
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-01-06 02:28:27 -0800

    Binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -8,27 +8,29 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int left = 0;
-    int right = size - 1;
-    int i = 0;
-    while (left <= right)
-    {
-        int m = (left + right) / 2;
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i == right)
-                printf("%d", array[i]);
-            else
-                printf("%d, ", array[i]);
-        }
-        printf("\n");
-        if (array[m] < value)
-            left = m + 1;
-        else if (array[m] > value)
-            right = m - 1;
-        else
-            return m;
-    }
-    return -1;
+	int left = 0;
+	int right = size - 1;
+	int i = 0;
+	int m = 0;
+
+	while (left <= right)
+	{
+		m = (left + right) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+				printf("%d", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		printf("\n");
+		if (array[m] < value)
+			left = m + 1;
+		else if (array[m] > value)
+			right = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
 }
