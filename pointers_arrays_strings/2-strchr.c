d4eb941a9fff940e96b4ee1f16ec5f305bf2420a
Hugo Chilemme <hugo.chilemme@gmail.com>
2024-11-04 13:44:33 +0000

    Optimize script

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
+	while (s[i] != '\0' && s[i] != c)
+		s++;
 
-	while (s[i] != '\0')
-	{
-		i++;
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-
-	}
-	return ('\0');
+	return (*s == c ? s : '\0');
 }
